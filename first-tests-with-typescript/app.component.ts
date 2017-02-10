6743de1c4ba20aafc12b794d27c2757760dfc061
unknown <benjamin.auzanneau@CZC50817F6.simat.local>
2017-02-10 14:25:01 +0100

    NS- Commencer de zéro à la main

---
+++ first-tests-with-typescript/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ponyracer-app', 
+    template : ' <h1>PonyRacer</h1> '
+}) 
+
+export class PonyRacerAppComponent {
+
+
+}
---
+++ first-tests-with-typescript/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { PonyRacerAppComponent } from './app.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [PonyRacerAppComponent],
+    bootstrap: [PonyRacerAppComponent]
+}
+
+export class AppModule{
+
+}
